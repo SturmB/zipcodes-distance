079547b4cb37412eb4f2ce3850d8b5a58dde1589
Chris McGee <chris@chrismcgee.info>
2022-02-11 17:31:06 -0500

    💄 Finishes first draft of UI

--- resources/views/zipcodes.blade.php
+++ resources/views/zipcodes.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Zip Code Distance Calculator</title>
 
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 </head>
@@ -14,6 +14,30 @@
     <div class="row">
         <div class="col">
             <h1 class="display-1">Zip Code Distance Calculator</h1>
+        </div>
+    </div>
+    <div class="row mt-5">
+        <div class="col mb-3">
+            <label for="zip-input">
+                <h3>Please enter a ZIP Code to add to your trip</h3>
+            </label>
+            <div class="input-group mb-3">
+                <input id="zip-input" type="text" class="form-control">
+                <button class="btn btn-primary" type="button">Submit</button>
+            </div>
+            <button class="btn btn-outline-danger" type="button">Start Over</button>
+        </div>
+        <div class="col mb-3">
+            <div class="card">
+                <div class="card-header">
+                    <h2 id="your-trip" class="card-title mt-2">Your Trip</h2>
+                </div>
+                <div class="card-body">
+                    <h4 class="card-text alert alert-primary">Total Distance: </h4>
+                    <label for="zip-list"><h3>The ZIP Codes you have entered</h3></label>
+                    <p id="zip-list" class="border border-1 rounded" readonly></p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
