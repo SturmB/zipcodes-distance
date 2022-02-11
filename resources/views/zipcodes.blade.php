6d817d611a797e8f64f088b231a6399481ee15c4
Chris McGee <chris@chrismcgee.info>
2022-02-11 16:50:45 -0500

    ➕ Adds Bootstrap

--- resources/views/zipcodes.blade.php
+++ resources/views/zipcodes.blade.php
@@ -6,14 +6,19 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 </head>
 <body>
 <div class="container">
     <div class="row">
         <div class="col">
-            <h1>Howdy</h1>
+            <h1 class="display-1">Zip Code Distance Calculator</h1>
         </div>
     </div>
 </div>
+
+<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+
 </body>
 </html>
